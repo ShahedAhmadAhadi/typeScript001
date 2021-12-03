5b8ccbf03ae6b1edb3cc318722ea8200ec2c91f0
Shahed Ahmad <shaheda.a16@gmail.com>
2021-12-03 22:58:52 +0430

    pattern for preventing wrong type of primitive added in TS

--- index.ts
+++ index.ts
@@ -2,12 +2,25 @@ import axios from "axios";
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1'
 
-axios.get(url).then(response =>{
-    const todo = response.data
+interface Todo {
+    id: number,
+    title: string,
+    completed: boolean
+}
 
-    const ID = todo.ID;
-    const title = todo.Title
-    const finished = todo.finished;
+axios.get(url).then(response => {
+    const todo = response.data as Todo
 
-    console.log(`The Todo with id: ${ID} Has a title of: ${title} Is it finished? ${finished}`)
+    const id = todo.id;
+    const title = todo.title
+    const completed = todo.completed;
+
+    logTodo(id, title, completed)
 })
+
+const logTodo = (id: number, title: string, completed: boolean) => {
+    console.log(`
+    The Todo with id: ${id}
+    Has a title of: ${title}
+    Is it finished? ${completed}`)
+}
