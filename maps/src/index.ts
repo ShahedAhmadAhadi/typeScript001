06346baedd930cec068576f91ad6775ee7f6ec55
Shahed Ahmad <shaheda.a16@gmail.com>
2021-12-06 00:46:03 +0430

    google types added, google api key for maps created and added and element to show company and show the maps

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,7 +1,13 @@
 import { User } from './User'
+import { Company } from './Company';
 
 const user = new User();
 
+const company = new Company()
+
 console.log(user)
+console.log(company)
+
+new google.maps.Map(document.getElementById('map'))
 
 
